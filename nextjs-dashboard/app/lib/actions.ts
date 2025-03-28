2290431d785f6ccee2dcff1e05ffc611c46443bb
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-03-28 10:31:14 +0200

    Remove unused redirect import and update button hover color for better UX

--- nextjs-dashboard/app/lib/actions.ts
+++ nextjs-dashboard/app/lib/actions.ts
@@ -3,7 +3,7 @@
 import { z } from 'zod';
 import postgres from 'postgres';
 import { revalidatePath } from 'next/cache';
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
 
