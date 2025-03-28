7d5785fcf0d8967bbf7d2ed53c9a3f78034f172e
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-03-28 16:51:14 +0200

    lint

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -2,7 +2,7 @@
 
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { Metadata } from 'next';
+// import { Metadata } from 'next';
 import { useEffect } from 'react';
 
 // export const metadata: Metadata = {
