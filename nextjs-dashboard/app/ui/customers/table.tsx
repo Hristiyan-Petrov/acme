d09812799599d5bd1b57802dad73d4703930f62c
Hristiyan Petrov <hristiyan_petrov@abv.bg>
2025-03-28 16:26:47 +0200

    Replace Image component with img tag for customer profile pictures configuration (mobile issue)

--- nextjs-dashboard/app/ui/customers/table.tsx
+++ nextjs-dashboard/app/ui/customers/table.tsx
@@ -91,7 +91,7 @@ export default async function CustomersTable({
                     <tr key={customer.id} className="group">
                       <td className="whitespace-nowrap bg-white py-5 pl-4 pr-3 text-sm text-black group-first-of-type:rounded-md group-last-of-type:rounded-md sm:pl-6">
                         <div className="flex items-center gap-3">
-                          <Image
+                          <img
                             src={customer.image_url}
                             className="rounded-full"
                             alt={`${customer.name}'s profile picture`}
